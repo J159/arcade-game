8fe7c5bf88526980afee261d983ccde44bc5c9c9
Andy Gaspar <agaspar.artist@gmail.com>
2018-10-24 03:48:11 -0400

    refactor Enemy class/variable to ES6 constructor

--- js/app.js
+++ js/app.js
@@ -1,25 +1,17 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Contructor function for Player and Enemy classes
+class PlayerEnemyConstruct {
+  constructor() {
+    // note: asign correct image path per Player and Enemy class
+    this.sprite = 'images/';
+    this.x = 0;
+    this.y = 0;
+  }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
+
 
 // Now write your own player class
 // This class requires an update(), render() and
