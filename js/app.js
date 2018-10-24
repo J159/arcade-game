1ce476cf3e70c90e19b8510a7cf8b82f3c256a2e
Andy Gaspar <agaspar.artist@gmail.com>
2018-10-24 03:54:56 -0400

    create new Player const for testing purposes

--- js/app.js
+++ js/app.js
@@ -19,15 +19,11 @@ class Player extends PlayerEnemyConstruct {
   }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
